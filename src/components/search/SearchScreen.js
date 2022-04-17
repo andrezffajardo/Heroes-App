ad4e582735b6e7e239ebef31f9dcb35fd7c6ef1f
Andres F. Fajardo <andrezffajardo@gmail.com>
2022-04-16 21:43:15 -0500

    Search component

--- src/components/search/SearchScreen.js
+++ src/components/search/SearchScreen.js
@@ -1,9 +1,56 @@
+import { useForm } from '../../hooks/useForm';
 
 
 export const SearchScreen = () => {
+
+    const [ formValues, handleInputChange ] = useForm({
+        searchText: '',
+    })
+
+    const { searchText } = formValues;
+
+    const handleSearch = (e) => {
+        e.preventDefault();
+        console.log(searchText);
+    }
+
     return (
-        <div>
-            <h1>SearchScreen</h1>
-        </div>
+        <>
+            <h1>Search</h1>
+            <hr />
+
+            <div className="row">
+
+                <div className="col-5">
+                    <h4>Search</h4>
+                    <hr />
+
+                    <form onSubmit={ handleSearch }>
+                        <input
+                            type="text"
+                            placeholder="Search Hero"
+                            className="form-control"
+                            name="searchText"
+                            autoComplete="off"
+                            value={ searchText }
+                            onChange={ handleInputChange }
+                        />
+
+                        <button
+                            className="btn btn-outline-primary mt-1"
+                            type="submit"
+
+                        >
+                            Search...
+                        </button>
+
+
+                    </form>
+
+
+                </div>
+
+            </div>
+        </>
     );
 };
