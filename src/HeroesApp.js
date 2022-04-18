fd53229147c54c093bb678193f886914245dbeae
Andres F. Fajardo <andrezffajardo@gmail.com>
2022-04-18 11:45:26 -0500

    useContext on app, get global state

--- src/HeroesApp.js
+++ src/HeroesApp.js
@@ -1,8 +1,23 @@
 import { AppRouter } from './routers/AppRouter';
+import { AuthContext } from './auth/authContext';
+import { authReducer } from './auth/authReducer';
+import { useReducer } from 'react';
 
+
+const init = () => {
+    return JSON.parse( localStorage.getItem('user') ) || { logged: false };
+}
 
 export const HeroesApp = () => {
+
+    const [ user, dispatch ] = useReducer( authReducer, {}, init )
+
     return (
-        <AppRouter />
+        <AuthContext.Provider value={{
+            user,
+            dispatch
+        }}>
+            <AppRouter />
+        </AuthContext.Provider>
     );
 };
