7548d14124966c81c5cc4144ee3f096395a14b62
Andres F. Fajardo <andrezffajardo@gmail.com>
2022-04-18 21:19:12 -0500

    Remember the last page visited

--- src/routers/PrivateRoute.js
+++ src/routers/PrivateRoute.js
@@ -1,11 +1,14 @@
 import { AuthContext } from '../auth/authContext';
 import { useContext } from 'react';
-import { Navigate } from 'react-router-dom';
+import { Navigate, useLocation } from 'react-router-dom';
 
 
 export const PrivateRoute = ({ children }) => {
 
     const { user } = useContext(AuthContext);
+    const { pathname, search } = useLocation();
+
+    localStorage.setItem( 'lastPath', pathname + search );
 
     return user.logged
         ? children
