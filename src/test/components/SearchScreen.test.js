bfa2ad38a90b1839c5694ca319e64321adcfaac1
Andres F. Fajardo <andrezffajardo@gmail.com>
2022-04-20 21:39:20 -0500

    Tests on SearchScreen - Test with queryParameters

--- src/test/components/SearchScreen.test.js
+++ src/test/components/SearchScreen.test.js
@@ -16,6 +16,32 @@ describe('Pruebas en <SearchScreen />', () => {
         expect( wrapper ).toMatchSnapshot();
         expect( wrapper.find('.alert-info').text().trim() ).toBe('Search a Hero');
 
-    })
+    });
+
+    test('Debe mostrar a Batman y el input con el valor del queryString', () => {
+
+        const wrapper = mount(
+            <MemoryRouter initialEntries={['/search?q=batman']}>
+                <SearchScreen />
+            </MemoryRouter>
+        );
+
+        expect( wrapper.find('input').prop('value') ).toBe('batman');
+        expect( wrapper ).toMatchSnapshot();
+
+    });
+
+    test('Debe mostrar un error si no se encuentra el Hero', () => {
+
+        const wrapper = mount(
+            <MemoryRouter initialEntries={['/search?q=batman123']}>
+                <SearchScreen />
+            </MemoryRouter>
+        );
+
+        expect( wrapper.find('.alert-danger').text().trim() ).toBe('without results: batman123');
+
+    });
+
 
 });
